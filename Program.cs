5fb5b984ae5f7e3cd8155c74479a0070da5d0ec9
Jordan Bryan <jbryan382@gmail.com>
2021-04-30 16:44:55 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RythmsGonnaGetYouExample.Models;
+using System.Linq;
+
+namespace RythmsGonnaGetYouExample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
